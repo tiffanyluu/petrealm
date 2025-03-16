feb1b9a66debe3b0f4acf52b9ca32f4400fa14e5
tiffanyluu <tiffanyluu555@gmail.com>
2025-03-16 17:53:04 -0500

    fix config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/index.html',
+          permanent: false,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
