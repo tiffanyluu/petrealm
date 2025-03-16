a250947fac6e65d99024c592564ca96f5abd4c9d
tiffanyluu <tiffanyluu555@gmail.com>
2025-03-16 18:25:55 -0500

    fix config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
