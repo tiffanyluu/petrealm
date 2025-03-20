969b91bd1f0ab9e0dead1c09a4ceeaa581f24886
tiffanyluu <tiffanyluu555@gmail.com>
2025-03-20 14:51:41 -0500

    edit config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Ensures Next.js exports static files
+    trailingSlash: true, // Helps with Amplify routing
+  };
+  
+  export default nextConfig;
+  
